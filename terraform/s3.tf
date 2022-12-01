2dcb9a2b8b015ff248cafca4cc840d25cbe08ae6
John Anderson <john@stacklet.com>
2022-12-01 15:50:37 -0400

    suggested permission change

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -15,44 +15,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "cur_bucket_encryp
   }
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${var.stacklet_saas_account_id}:role/${var.customer_prefix}-cur-read"]
-    }
-  }
-}
-resource "aws_iam_policy" "stacklet-shared-cur-s3-access" {
-  name   = "stacklet-shared-cur-s3-access"
-  path   = "/"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-          "s3:ListBucket"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${local.s3_bucket_name}"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-          "s3:GetObject"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${local.s3_bucket_name}/*"
-      ]
-    }
-  ]
-}
-EOF
-}
 resource "aws_s3_bucket_policy" "s3-bucket-cur-report-policy" {
   bucket = aws_s3_bucket.cur_bucket.id
   policy = <<EOF
@@ -77,16 +39,24 @@ resource "aws_s3_bucket_policy" "s3-bucket-cur-report-policy" {
       },
       "Action": "s3:PutObject",
       "Resource": "arn:aws:s3:::${local.s3_bucket_name}/*"
+    },
+    {
+     "Effect": "Allow",
+     "Principal": {
+         "AWS": "arn:aws:iam::${var.stacklet_saas_account_id}:${var.customer_prefix}-cur-read"
+     },
+     "Action": "s3:ListBucket",
+     "Resource": "arn:aws:s3:::${local.s3_bucket_name}"
+    },
+    {
+        "Effect": "Allow",
+        "Principal": {
+            "AWS": "arn:aws:iam::${var.stacklet_saas_account_id}:${var.customer_prefix}-cur-read"
+        },
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::${local.s3_bucket_name}/*"
     }
   ]
 }
 EOF
-}
-resource "aws_iam_role" "stacklet-shared-cur-access-role" {
-  name                = "stacklet-shared-cur-access"
-  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
-}
-resource "aws_iam_role_policy_attachment" "cur-s3-access" {
-  role       = aws_iam_role.stacklet-shared-cur-access-role.name
-  policy_arn = aws_iam_policy.stacklet-shared-cur-s3-access.arn
 }
