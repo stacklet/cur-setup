1041ea49ca90589ba3dddcb51427799ef32bb74b
James Troup <github@elmo.tasta.io>
2026-04-06 15:27:02 +0100

    chore: add CI, pre-commit, dependabot, and bump provider versions (#11)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.13.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
